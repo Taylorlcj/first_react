526d60f40c6202616165ae5b2d80c387c66a3ec5
Taylor Jensen <Taylorlcj@gmail.com>
2022-11-21 00:21:42 -0500

    nonsense

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Taylor Jensen
+          Taylor Jensen first React App
         </a>
       </header>
     </div>
